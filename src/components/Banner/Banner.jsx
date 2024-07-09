3d2fc0f05d2d061e6dfd338465ca470acbcd27d4
Samuel Ferreira <contato.ferreirasamuel@gmail.com>
2024-07-09 15:09:15 -0300

    initalizing tailwind

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import banner from "../../assets/banner.webp"
 import arrow from "../../assets/bannerArrow.svg"
 import "./styles.css"
 
-const scrollToComponent = () => {
+const scrollToComponent = () => {  // white
   window.scrollBy({
     top: window.innerHeight * 0.8,
     behavior: 'smooth'
