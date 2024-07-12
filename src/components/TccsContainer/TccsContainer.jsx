7cd22800f27e3edb62a3b2cd698dd26d9b93e168
Samuel Ferreira <contato.ferreirasamuel@gmail.com>
2024-07-11 23:42:22 -0300

    tailwind

--- src/components/TccsContainer/TccsContainer.jsx
+++ src/components/TccsContainer/TccsContainer.jsx
@@ -6,9 +6,9 @@ import { TccsCard } from "./TccsCard/TccsCard"
 export function TccsContainer() {
   return (
     <div className="tccs-container flex flex-col items-center justify-center text-center leading-5 gap-8 text-black70 mx-[20vw]">
-      <h1 className="home-title text-theme text-4xl -mb-2">TCCs</h1>
-      <p className="home-subtitle">Aqui, estudantes e pesquisadores encontram uma variedade de recursos e inspirações para a elaborção de seus Trabalhos de Conclusão de Curso (TCCs) focados na suinocultura. Oferecemos orientações detalhadas, exemplos de projetos bem-sucedidos, e acesso a uma comunidade de especialistas prontos para compartihar conhecimentos e apoiar o desenvolvimento de novas pesquisas no setor. </p>
-      <div className="tccs-cards-container">
+      <h1 className="text-theme text-4xl -mb-2">TCCs</h1>
+      <p className="text-2xl">Aqui, estudantes e pesquisadores encontram uma variedade de recursos e inspirações para a elaborção de seus Trabalhos de Conclusão de Curso (TCCs) focados na suinocultura. Oferecemos orientações detalhadas, exemplos de projetos bem-sucedidos, e acesso a uma comunidade de especialistas prontos para compartihar conhecimentos e apoiar o desenvolvimento de novas pesquisas no setor. </p>
+      <div className="tccs-cards-container grid grid-cols-2 items-center gap-y-8 gap-x-20 mx-[10vw]">
         <TccsCard />
         <TccsCard />
         <TccsCard />
