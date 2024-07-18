9346dfb7814b2a1536738e95fdefe414f637105d
Samuel Ferreira <contato.ferreirasamuel@gmail.com>
2024-07-18 09:45:59 -0300

    responsive navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,20 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 // import "./styles.css"
 import { NavBar } from "./NavBar/NavBar"
 import { Logo } from "./Logo/Logo"
 import { MobileMenu } from "./MobileMenu/MobileMenu"
 import { UserButton } from "./UserButton/UserButton"
+import { useScreenSize } from "../../hooks/useScreenSize"
 
 export function Header() {
+
+  const screenSize = useScreenSize(); 
+
   return (
     <header className=" text-white absolute flex justify-between items-center h-[10vh] w-full px-12 py-[5vw] z-10">
       <Logo />
       <div className="flex gap-4">
-        <NavBar />
-      <MobileMenu />
+        {screenSize > 720 ? <NavBar /> : <MobileMenu />}
         <UserButton />
       </div>
     </header>
---
+++ src/hooks/useScreenSize.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react"
+
+export const useScreenSize = () => {
+    const [screenSize, setScreenSize] = useState(window.innerWidth)
+    
+      useEffect(() => {
+        const handleResize = () => {
+          setScreenSize(window.innerWidth);
+        }
+        console.log(screenSize);
+        
+        window.addEventListener('resize', handleResize);
+    
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        }
+      }, [])
+
+    return screenSize;
+}
